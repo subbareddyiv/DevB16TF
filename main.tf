eb7c4662df7b8bf26460d03c65964d089169d628
Venkat <subbareddyiv12@gmail.com>
2025-11-20 13:49:37 +0000

    Base Provider & VPC Code

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    
+    region = "usa-east-1"
+}
+resource "aws_vpc" "default" {
+    cidr_block = "172.19.0.0/16"
+    enable_dns_hostnames = true
+    tags = {
+        Name = "DevB16Git"
+	Owner = "Venkat"
+	environment = "Development"
+    }
+}
